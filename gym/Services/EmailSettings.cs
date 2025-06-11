0d05699e8b120c842d1e75ff10d6cac68b1e8adb
Lê Duy Hải <leduyhai090704@gmail.com>
2025-06-11 20:44:09 +0700

    reset

--- gym/Services/EmailSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿public class EmailSettings
-{
-    public string SmtpServer { get; set; } = string.Empty;
-    public int SmtpPort { get; set; }
-    public string SenderName { get; set; } = string.Empty;
-    public string SenderEmail { get; set; } = string.Empty;
-    public string SenderPassword { get; set; } = string.Empty;
-}
